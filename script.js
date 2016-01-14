9eca84633b38d20087ea15a0892d5adb7c203971
Lily Zhang <Lily@Lilys-MacBook-Air.local>
2016-01-13 22:17:43 -0500

    WHEE

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
 "<li><a href='index.html'>Home</a></li>" +
-"<li><a href=‘About.html'>About</a></li>" +
+"<li><a href=‘About.html’>About</a></li>" +
 "</ul>";
